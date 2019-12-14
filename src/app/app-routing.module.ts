944d301e6a589a1323291bf31d92620620c71b74
kyle <zldesu@163.com>
2019-12-14 12:49:50 +0800

    git@github.com:zldesu/resume.git

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GreetingComponent } from './greeting/greeting.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'greet', component: GreetingComponent },
+  { path: '', redirectTo: 'greet', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GreetingComponent } from './greeting/greeting.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GreetingComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
